dc171562aa2a5eafdc54acaf41a0252aaae11cc5
djviau <dan@opensea.io>
2023-09-21 15:49:06 -0400

    fix linting

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import "forge-std/Script.sol";
 
-import { ExampleNFT } from "shipyard-core/reference/ExampleNFT.sol";
+import {ExampleNFT} from "shipyard-core/reference/ExampleNFT.sol";
 
 import "forge-std/console.sol";
 
